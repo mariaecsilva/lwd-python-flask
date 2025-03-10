4bf5e2c9a4c61d7f1b1ff90d8cd2197cdba48aa2
Maria Eduarda <145166801+maducsilva@users.noreply.github.com>
2025-03-10 16:58:44 -0300

    add updated activity

--- atividades/tarefa1-aplicao-flask/controllers/routes.py
+++ atividades/tarefa1-aplicao-flask/controllers/routes.py
@@ -1,9 +1,8 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for;
 import urllib
 import json
 
-productlist = []  
-product_id_counter = 1  
+records=[]
 
 def init_app(app):
 
@@ -11,51 +10,36 @@ def init_app(app):
     def home():
         return render_template('index.html')
 
-    @app.route('/catalog', methods=['GET'])
-    def catalog():
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if request.method == 'POST':
+            title = request.form.get('title')
+            price = request.form.get('price')
+            time = request.form.get('time')
+
+            if request.form.get('title'):
+                records.append({'title': title, 'price': price, 'time': time})
+                return redirect(url_for('register'))
+        
+        return render_template('register.html', records=records)
+
+    @app.route('/catalog', methods=['GET', 'POST']) 
+    @app.route('/catalog/<int:id>', methods=['GET', 'SET'])
+    def catalog(id=None): 
         url = 'https://fakestoreapi.com/products'
         headers = {'User-Agent': 'Mozilla/5.0'}
         req = urllib.request.Request(url, headers=headers)
-        try:
-            response = urllib.request.urlopen(req)
+        with urllib.request.urlopen(req) as response:
             data = response.read()
             productsjson = json.loads(data)
-        except urllib.error.HTTPError as e:
-            return f"Erro ao acessar a API: {e}"
-
-        all_products = productsjson + productlist
-        
-        return render_template('catalog.html', productsjson=all_products)
-
-    @app.route('/register', methods=['GET', 'POST'])
-    def register():
-        global product_id_counter 
-        if request.method == 'POST':
-            title = request.form.get('title')
-            value = request.form.get('value')
-            category = request.form.get('category')
-            description = request.form.get('description')
-            image = request.form.get('image')
-            
-            if title and value and category and image:
-                productlist.append({
-                    'id': product_id_counter, 
-                    'title': title,
-                    'value': value,
-                    'category': category,
-                    'description': description,
-                    'image': image
-                })
-                product_id_counter += 1 
-            return redirect(url_for('catalog'))  
-
-        return render_template('register.html', productlist=productlist)
-
-    @app.route('/product/<int:id>', methods=['GET'])
-    def product(id):
-        product_info = next((prod for prod in productlist if prod['id'] == id), None)
-
-        if product_info:
-            return render_template('product.html', productInfo=product_info)
-        else:
-            return f'Produto com a id {id} não encontrado.'
+        if id:
+            product=[]
+            for product in productsjson:
+                if product ['id'] == id:
+                    product=product
+                    break
+                if product:
+                    return render_template('product.html', product=product)
+                else: 
+                    return f'Produto com a id {id} não encontrado.' 
+        return render_template('catalog.html', productsjson=productsjson)
